47d9ad31f3fd314da1141e662d7aa9732b90997a
abray <armando.ameri@studio.unibo.it>
2024-01-16 11:36:19 +0100

    end hero

--- meta-front-end-capstone/src/components/Main.js
+++ meta-front-end-capstone/src/components/Main.js
@@ -1,4 +1,6 @@
+import image from '../assets/icons_assets/restauranfood.jpg'
 
+import Button from "./Button";
 
 function Main() {
     return (
@@ -7,7 +9,8 @@ function Main() {
           <h1 className="title">Little Lemon</h1>
           <h2 className="subtitle">Chicago</h2>
           <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestias totam aut tenetur, laborum illum eveniet inventore reprehenderit iusto error, praesentium ipsam delectus sequi natus a itaque cum cupiditate magni sunt!</p>
-
+          <Button name="Reserve a Table"/>
+          <img src={image} alt="restauran food image" />
         </div>
         <div style={{backgroundColor: "red", height:'300px', width:'300px'}}>Box 1</div>
         <div style={{backgroundColor: "red", height:'300px', width:'300px'}}>Box 1</div>
